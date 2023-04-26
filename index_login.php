5eb71d57871002cbe90bdf34b24e7d73dc65aca3
SelaruNatalia <100343312+SelaruNatalia@users.noreply.github.com>
2023-04-26 22:57:12 +0300

    Create index_login.php

---
+++ index_login.php
@@ -0,0 +1,27 @@
+<?php
+	include ('login.php')
+?>
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="widt=device-width,initial-scale=1.0">
+	<title>Logheaza-te</title>
+	<link rel="stylesheet" type="text/css" href="login_style.css">
+</head>
+<body>
+	<div id="form">
+		<h1>Logheaza-te</h1>
+	<form name="form" action="login.php" onsubmit="return isvalid()" method="POST">
+		<label>Email:</label> 
+		<input type="email" id="email" name="email"><br><br>
+		<label>Parola:</label>
+		<input type="password" id="pass" name="pass"><br><br>
+		
+		<input type="submit" id="btn" name="submit" value="Login"/>
+		<p style="font-size: 20px;">Nu ai cont?<a href="register.php">Inregistreaza-te acum</a></p>
+	</form>
+	</div>
+</body>
+</html>
