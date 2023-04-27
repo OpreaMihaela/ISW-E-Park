88458ae302b8627d5b3890266cf9cbf78190650c
SelaruNatalia <100343312+SelaruNatalia@users.noreply.github.com>
2023-04-27 11:19:36 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,8 +2,8 @@
     $servername = "localhost";
     $email = "root";
     $pass = "root";
-    $db_name = "database1";
-    $conn = new mysqli($servername, $email, $pass, $db_name);
+    $db_name = "user";
+    $conn = new mysqli($servername, $email, $pass, $db_name, 3306);
     if($conn -> connect_error){
         die("Connection failed".$conn -> connect_error);
     }
